99c6318e91d14b1cadaeeba6d2fdc76424d4e43d
Paulo Henrique Alves Teixeira <rick.ateixeira@hotmail.com>
2025-11-30 16:37:40 -0300

    estilização filtro por matéria

--- src/features/subjects/components/ActionsContainer/ActionsContainer.jsx
+++ src/features/subjects/components/ActionsContainer/ActionsContainer.jsx
@@ -1,6 +1,5 @@
 import Button from "../../../../components/Button/Button";
 import { Plus } from "lucide-react";
-import FilterContainer from "../../../../components/FilterContainer/FilterContainer";
 import style from "./ActionsContainer.module.css";
 
 export default function ActionsContainer({onNewSubject }) {
